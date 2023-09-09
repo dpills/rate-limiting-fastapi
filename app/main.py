2863ffb54c61e32c0715e43e6ae61409daf097ae
dpills <dpills@protonmail.com>
2023-09-09 16:37:59 -0400

    add app path

--- app/main.py
+++ app/main.py
@@ -93,7 +93,7 @@ def get_user(x_user: Annotated[str | None, Header()] = None):
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "app.main:app",
         host="0.0.0.0",
         port=8000,
         log_level="debug",
