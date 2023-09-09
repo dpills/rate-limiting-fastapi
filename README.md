3504f9a9d27a906f83b4fd0a98d54b97be6ff229
dpills <dpills@protonmail.com>
2023-09-09 16:15:44 -0400

    Example setup

---
+++ README.md
@@ -0,0 +1,23 @@
+# FastAPI Rate Limiting
+
+This project shows a basic example of how to implement a rate limiter in FastAPI with Redis. Run this app with Docker Compose to test it out locally.
+
+```bash
+$ docker-compose up
+
+rate-limiting-fastapi-redis-1  | 1:C 09 Sep 2023 20:08:23.209 * oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo
+rate-limiting-fastapi-redis-1  | 1:C 09 Sep 2023 20:08:23.209 * Redis version=7.2.1, bits=64, commit=00000000, modified=0, pid=1, just started
+rate-limiting-fastapi-redis-1  | 1:C 09 Sep 2023 20:08:23.209 * Configuration loaded
+rate-limiting-fastapi-redis-1  | 1:M 09 Sep 2023 20:08:23.209 * monotonic clock: POSIX clock_gettime
+rate-limiting-fastapi-redis-1  | 1:M 09 Sep 2023 20:08:23.209 * Running mode=standalone, port=6379.
+rate-limiting-fastapi-redis-1  | 1:M 09 Sep 2023 20:08:23.210 * Server initialized
+rate-limiting-fastapi-redis-1  | 1:M 09 Sep 2023 20:08:23.210 * Ready to accept connections tcp
+
+rate-limiting-fastapi-api-1    | INFO:     Started server process [1]
+rate-limiting-fastapi-api-1    | INFO:     Waiting for application startup.
+rate-limiting-fastapi-api-1    | INFO:     Application startup complete.
+rate-limiting-fastapi-api-1    | INFO:     Uvicorn running on http://0.0.0.0:8000 (Press CTRL+C to quit)
+rate-limiting-fastapi-api-1    | INFO:     172.21.0.1:39136 - "GET / HTTP/1.1" 200 OK
+rate-limiting-fastapi-api-1    | INFO:     172.21.0.1:39136 - "GET /openapi.json HTTP/1.1" 200 OK
+rate-limiting-fastapi-api-1    | INFO:     172.21.0.1:58024 - "GET /user HTTP/1.1" 200 OK
+```
---
+++ app/main.py
@@ -0,0 +1,101 @@
+import hashlib
+from datetime import datetime, timedelta
+from typing import Annotated, Any, Callable, TypeVar
+
+import uvicorn
+from fastapi import FastAPI, Header, Request, Response
+from fastapi.responses import JSONResponse
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from redis.asyncio import Redis
+
+F = TypeVar("F", bound=Callable[..., Any])
+
+
+class Settings(BaseSettings):
+    redis_password: str
+    redis_host: str = "127.0.0.1"
+    redis_port: int = 6379
+    user_rate_limit_per_minute: int = 3
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+settings = Settings()
+
+redis_client = Redis(
+    host=settings.redis_host,
+    port=settings.redis_port,
+    db=0,
+    decode_responses=True,
+    password=settings.redis_password,
+)
+app = FastAPI(
+    title="FastAPI Rate Limiting",
+    description="Rate limiting users using Redis middleware",
+    docs_url="/",
+)
+
+
+async def rate_limit_user(user: str, rate_limit: int) -> JSONResponse | None:
+    """
+    Apply rate limiting per user, per minute
+    """
+    # Increment our most recent redis key
+    username_hash = hashlib.sha256(bytes(user, "utf-8")).hexdigest()
+    now = datetime.utcnow()
+    current_minute = now.strftime("%Y-%m-%dT%H:%M")
+
+    redis_key = f"rate_limit_{username_hash}_{current_minute}"
+    current_count = await redis_client.incr(redis_key)
+
+    # If we just created a new key (count is 1) set an expiration
+    if current_count == 1:
+        await redis_client.expireat(name=redis_key, when=now + timedelta(minutes=1))
+
+    # Check rate limit
+    if current_count > rate_limit:
+        return JSONResponse(
+            status_code=429,
+            content={"detail": "User Rate Limit Exceeded"},
+            headers={
+                "Retry-After": f"{60 - now.second}",
+                "X-Rate-Limit": f"{rate_limit}",
+            },
+        )
+
+    return None
+
+
+@app.middleware("http")
+async def rate_limit_middleware(request: Request, call_next: F) -> Response:
+    """
+    Rate limit requests per user
+    """
+    user = request.headers.get("x-user")
+    if user:
+        rate_limit_exceeded_response = await rate_limit_user(
+            user=user, rate_limit=settings.user_rate_limit_per_minute
+        )
+        if rate_limit_exceeded_response:
+            return rate_limit_exceeded_response
+
+    return await call_next(request)
+
+
+@app.get("/user", response_model=str | None)
+def get_user(x_user: Annotated[str | None, Header()] = None):
+    """
+    **NOTE**: The `X-User` header should be passed by a reverse proxy,
+    we are manually adding it to this endpoint so you can test
+    this example locally
+    """
+    return x_user
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_level="debug",
+        reload=True,
+    )
